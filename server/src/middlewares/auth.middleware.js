6d8a19080123f0593ba4a5c1f45a6f01307903c1
Ranjeet <ranjeeturd@gmail.com>
2025-06-16 18:30:38 +0530

    Fixes bugs in validator and auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Admin from "../models/admin.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
@@ -15,15 +16,19 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        // find user on the basis of token
-        const user = await User.findById(decodedToken?._id).select(" -password -refreshToken");
+        // First try to find user
+        let user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+        // If user not found, try to find admin
+        if (!user) {
+            user = await Admin.findById(decodedToken?._id).select("-password -refreshToken");
+        }
 
         if (!user) {
             throw new ApiError(401, "Invalid Access token");
         }
 
         req.user = user;
-
         next(); 
 
     } catch (error) {
--- server/src/middlewares/validator.middleware.js
+++ server/src/middlewares/validator.middleware.js
@@ -1,19 +1,29 @@
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
 
+export const validateObjectId = (req, res, next) => {
+    const { id } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new ApiError(400, "Invalid ID format");
+    }
+    
+    next();
+};
+
+// Generic validator middleware
 export const validator = (req, res, next) => {
     const errors = validationResult(req);
-
-    if(errors.isEmpty()) {
-        return next();
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map(err => {
+            // If the error is for a conditional field, make the message clearer
+            if (err.type === 'field') {
+                return `${err.path}: ${err.msg}`;
+            }
+            return err.msg;
+        });
+        throw new ApiError(400, errorMessages.join(", "));
     }
-
-    const extractedErrors = [];
-
-    errors.array().map((err) => extractedErrors.push({
-        [err.param]: err.msg
-    }))
-
-
-    throw new ApiError(422, "Received data is not valid", extractedErrors);
-}
+    next();
+};
