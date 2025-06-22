ccebae9fe5bf0fa6d074703202463ef67924dbb3
Ranjeet <ranjeeturd@gmail.com>
2025-06-22 22:32:51 +0530

    setup main.jsx page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '@/index.css'
+import App from '@/App'
+import store from '@/store/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from '@/components/theme-provider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <App />
+          {/* <Toaster position="bottom-right" /> */}
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
